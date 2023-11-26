d5d343b99e8d1f616471884f7fd0b76219411743
Visibyte <nathanglass26@gmail.com>
2023-11-26 16:38:02 +0000

    use navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "700"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={cn("relative h-full antialiased", roboto.className)}>
         {/* Prevents footer from creeping up the page */}
+        <Navbar />
         <main className="relative flex flex-col min-h-screen">
           <div className="flex-grow flex-1">{children}</div>
         </main>
