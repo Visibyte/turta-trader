e1490faee653001d08ee873600661cb064fbfdc8
Visibyte <nathanglass26@gmail.com>
2023-11-27 11:07:04 +0000

    static cart functionality

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -4,12 +4,19 @@ import { ShoppingBag } from "lucide-react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 const Cart = () => {
+  const itemCount = 1;
+  const transactionFee = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -25,6 +32,44 @@ const Cart = () => {
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart (0)</SheetTitle>
         </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              Cart items will go here
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-2 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Delivery Time</span>
+                  <span>Instant</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(transactionFee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Cart Total</span>
+                  <span>{formatPrice(transactionFee)}</span>
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full",
+                    })}
+                  >
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
       </SheetContent>
     </Sheet>
   );
