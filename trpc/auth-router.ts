3f08ff57c33d2079890cc9529cb16e24e4b59a12
Visibyte <nathanglass26@gmail.com>
2023-11-27 17:58:39 +0000

    validate email procedure

--- trpc/auth-router.ts
+++ trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from "../lib/validators/auth-credentials";
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload-client";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const authRouter = router({
   createUser: publicProcedure
@@ -29,10 +30,27 @@ export const authRouter = router({
         data: {
           email,
           password,
-          role: "user"
+          role: "user",
         },
       });
 
-      return {success: true, sentToEmail: email}
+      return { success: true, sentToEmail: email };
+    }),
+
+  validateEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .mutation(async ({ input }) => {
+      const { token } = input;
+
+      const payload = await getPayloadClient();
+
+      const isVerified = await payload.verifyEmail({
+        collection: "users",
+        token,
+      });
+
+      if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+      return { success: true };
     }),
 });
