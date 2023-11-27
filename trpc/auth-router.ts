ad511bd99bf457cc453387c27080b72cf5451220
Visibyte <nathanglass26@gmail.com>
2023-11-27 19:45:25 +0000

    sign in procedure

--- trpc/auth-router.ts
+++ trpc/auth-router.ts
@@ -55,4 +55,29 @@ export const authRouter = router({
 
       return { success: true };
     }),
+
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const email = input.email.toLowerCase();
+      const { password } = input;
+      const { res } = ctx;
+
+      const payload = await getPayloadClient();
+
+      try {
+        await payload.login({
+          collection: "users",
+          data: {
+            email,
+            password,
+          },
+          res,
+        });
+
+        return { success: true };
+      } catch (error) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+    }),
 });
