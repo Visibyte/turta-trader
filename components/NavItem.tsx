66b5c52e186dfe34459507a81ac319a1c7589d5d
Visibyte <nathanglass26@gmail.com>
2023-11-27 10:28:52 +0000

    nav item(s)

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { CATEGORIES } from "@/utils/Categories";
 import { Button } from "./ui/button";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronRight } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 type Category = (typeof CATEGORIES)[number];
 
@@ -35,6 +37,53 @@ const NavItem = ({
           />
         </Button>
       </div>
+      {isActive ? (
+        <div
+          className={cn(
+            "absolute inset-x-0 top-full text-sm text-muted-foreground",
+            {
+              "animate-in fade-in-10 slide-in-fro-top-5": !isAnyActive,
+            }
+          )}
+        >
+          <div
+            className="absolute inset-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          />
+          <div className="relative bg-white">
+            <div className="mx-auto max-w-7xl px-8">
+              <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                  {category.featured.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative text-base sm:text-sm"
+                    >
+                      <Link href={item.href}>
+                        <div className="relative aspect-video overflow-hidden rounded-md group-hover:opacity-75">
+                          <Image
+                            src={item.imageSrc}
+                            fill
+                            alt={item.name}
+                            className="object-cover object-center"
+                          />
+                        </div>
+                        <span className="mt-6 flex items-center text-base font-medium text-slate-900">
+                          {item.name}
+                          <ChevronRight className="text-muted-foreground w-4 h-4 mb-0.5" />
+                        </span>
+                        <p className="mt-1" aria-hidden="true">
+                          Shop {item.name} {item.tag}
+                        </p>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,15 +1,32 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CATEGORIES } from "@/utils/Categories";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/UseOnClickOutside";
 
 const NavItems = () => {
   const [activeCategory, setActiveCategory] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      e.key === "Escape" && setActiveCategory(null);
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   const isAnyActive = activeCategory !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveCategory(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {CATEGORIES.map((category, index) => {
         const handleActive = () => {
           activeCategory === index
