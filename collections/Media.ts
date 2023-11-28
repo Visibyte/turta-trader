1d444d4436fccc0aa1dc581d4ec27bb08e648909
Visibyte <nathanglass26@gmail.com>
2023-11-28 13:34:17 +0000

    media user based access

--- collections/Media.ts
+++ collections/Media.ts
@@ -1,4 +1,22 @@
-import { CollectionConfig } from "payload/types";
+import { User } from "../payload-types";
+import { Access, CollectionConfig } from "payload/types";
+
+const isAdminOrHasAccess =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined;
+
+    if (!user) return false;
+
+    if (user.role === "admin") return true;
+
+    // If the id's match, this user owns the image and therefore can see it
+    return {
+      user: {
+        equals: req.user.id,
+      },
+    };
+  };
 
 export const Media: CollectionConfig = {
   slug: "media",
@@ -9,6 +27,19 @@ export const Media: CollectionConfig = {
         return { ...data, user: req.user.id };
       },
     ],
+  },
+  access: {
+    read: async ({ req }) => {
+      const referer = req.headers.referer;
+
+      if (!req.user || !referer?.includes("sell")) {
+        return true;
+      }
+
+      return await isAdminOrHasAccess()({ req });
+    },
+    delete: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
   },
   admin: {
     hidden: ({ user }) => user.role !== "admin",
