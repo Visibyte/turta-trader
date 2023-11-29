771b17aee42f60da54a3def78b0eb68e0bf3b050
Visibyte <nathanglass26@gmail.com>
2023-11-29 19:32:04 +0000

    update product reel

--- components/ProductReel.tsx
+++ components/ProductReel.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { TQueryValidator } from "@/lib/validators/query-validator";
+import { Product } from "@/payload-types";
 import { trpc } from "@/trpc/client";
 import Link from "next/link";
+import ProductListing from "./ProductListing";
 
 interface ProductReelProps {
   title: string;
@@ -15,7 +17,7 @@ const FALLBACK_LIMIT = 4;
 const ProductReel = (props: ProductReelProps) => {
   const { title, subtitle, href, query } = props;
 
-  const { data } = trpc.getProducts.useInfiniteQuery(
+  const { data: queryResults, isLoading } = trpc.getProducts.useInfiniteQuery(
     {
       limit: query.limit ?? FALLBACK_LIMIT,
       query,
@@ -25,7 +27,15 @@ const ProductReel = (props: ProductReelProps) => {
     }
   );
 
-  console.log("data:", data);
+  const products = queryResults?.pages.flatMap((page) => page.items);
+
+  let map: (Product | null)[] = [];
+
+  if (products && products.length) {
+    map = products;
+  } else if (isLoading) {
+    map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null);
+  }
 
   return (
     <section className="py-12">
@@ -51,7 +61,11 @@ const ProductReel = (props: ProductReelProps) => {
       </div>
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
-          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 lg:gap-x-8"></div>
+          <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 lg:gap-x-8">
+            {map.map((product, index) => (
+              <ProductListing key={index} product={product} index={index} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
