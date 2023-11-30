0818356c35da2130854e2741443651003471a7be
Visibyte <nathanglass26@gmail.com>
2023-11-30 20:26:31 +0000

    resend and body parser

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import { nextApp, nextHandler } from "./next-utilts";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
+import bodyParser from "body-parser";
+import { IncomingMessage } from "http";
+import { stripeWebhookHandler } from "./webhooks";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -18,7 +21,20 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
+
 const startServer = async () => {
+
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    }
+  })
+
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
