a96eb2b234a81dfd981c647f979c362c9edc6af2
Visibyte <nathanglass26@gmail.com>
2023-12-01 10:00:18 +0000

    email webhook

--- webhooks.ts
+++ webhooks.ts
@@ -5,6 +5,7 @@ import { stripe } from "./lib/stripe";
 import { getPayloadClient } from "./get-payload-client";
 import { Product } from "./payload-types";
 import { Resend } from "resend";
+import { ReceiptEmailHtml } from "./components/emails/ReceiptEmail";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
